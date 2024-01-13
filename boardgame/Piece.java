239f28f11403b505779ad85bd44a2c181b958073
Luiz Neto <luiznetopb@gmail.com>
2024-01-13 11:04:25 -0300

    add class Piece

---
+++ boardgame/Piece.java
@@ -0,0 +1,24 @@
+package boardgame;
+
+public class Piece {
+
+    private Position pos;
+
+    public Piece(Position pos) {
+        this.pos = pos;
+    }
+
+    public Position getPos() {
+        return pos;
+    }
+
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
+
+//     boolean possibleMoves(){}
+
+//    boolean possibleMove(Position pos){}
+
+//    boolean isThereAnyPossibleMove() {}
+}
